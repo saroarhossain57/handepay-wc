89e04172bba11cdc22e2b673a5df38dd7bc25222
Saroar Hossain <limonhossain57@gmail.com>
2023-12-17 00:39:44 +0600

    Added basic plugin structure

---
+++ handepay-wc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Handepay WooCommerce Payment Gateway
+ * Description: A WooCommerce payment gateway for Handepay.
+ * Version: 1.0.0
+ * Author: Saroar Hossain
+ * Author URI: https://saroar.me
+ * Text Domain: handepay-wc
+ * Domain Path: /languages
+ * License: GPLv3+
+ * Requires at least: 5.6
+ * Requires PHP: 7.4
+ * 
+ * @package HandepayWC
+ */
+
+
+ defined( 'ABSPATH' ) || exit;
+
+
+ require_once __DIR__ . '/vendor/autoload.php';
