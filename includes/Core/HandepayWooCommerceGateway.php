67cb7089e4a136d78d74edae12e50d48f803f6ea
Saroar Hossain <limonhossain57@gmail.com>
2023-12-17 12:27:38 +0600

    Added: Payment settings fields

--- includes/Core/HandepayWooCommerceGateway.php
+++ includes/Core/HandepayWooCommerceGateway.php
@@ -14,6 +14,44 @@ class HandepayWooCommerceGateway extends \WC_Payment_Gateway {
 
         $this->init_form_fields();
         $this->init_settings();
+
+        $this->title = $this->get_option('title');
+        $this->description = $this->get_option('description');
+
+        // Add hooks
+        add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
     }
 
+
+    // Add gateway settings
+    public function init_form_fields(){
+
+        $this->form_fields = array(
+            'enabled' => array(
+                'title'       => 'Enable/Disable',
+                'label'       => 'Enable HandePay Payment Gateway',
+                'type'        => 'checkbox',
+                'description' => '',
+                'default'     => 'no',
+            ),
+            'title' => array(
+                'title'       => 'Title',
+                'type'        => 'text',
+                'description' => 'This controls the title which the user sees during checkout.',
+                'default'     => 'HandePay',
+                'desc_tip'    => true,
+            ),
+            'description' => array(
+                'title'       => 'Description',
+                'type'        => 'textarea',
+                'description' => 'This controls the description which the user sees during checkout.',
+                'default'     => 'Pay with HandePay',
+                'desc_tip'    => true,
+            )
+        );
+    }
+
+    public function process_payment($order_id){
+        
+    }
 }
